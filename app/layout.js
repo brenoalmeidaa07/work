eca476ace2181752e05ddbc86850fa1cd6a0507b
brenoalmeidaa07 <121868493+brenoalmeidaa07@users.noreply.github.com>
2023-09-01 15:38:04 -0300

    work

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -10,7 +9,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt">
       <body className={inter.className}>{children}</body>
     </html>
   )
